57b81edfd70bf33f9c285e6de959fff31c4c8e5c
Jason McCarrell <jason.abz@gmail.com>
2016-10-13 12:14:13 -0400

    debounced warning

--- mdvariables.js
+++ mdvariables.js
@@ -1,6 +1,7 @@
 var Plugin = require('markdown-it-regexp');
 
 var MdVariables = function(dataFn) {
+    var timeout;
     return Plugin(
         /@(\w+)/,
         function(match, utils) {
@@ -8,7 +9,11 @@ var MdVariables = function(dataFn) {
             if(data[match[1]])
                 return data[match[1]];
             else {
-                console.warn('MdVariables: We couldn\'t find the variable from the data callback. (key, data)', match[1], data);
+                if(timeout)
+                    clearTimeout(timeout);
+                timeout = setTimeout(function() {
+                    console.warn('MdVariables: We couldn\'t find the variable from the data callback. (key, data)', match[1], data);
+                }, 500);
                 return '';
             }
         }
